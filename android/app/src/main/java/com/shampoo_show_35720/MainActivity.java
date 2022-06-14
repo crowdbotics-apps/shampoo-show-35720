86a796bfa3ea315624cb2ed7a57df1568afae58f
Crowdbotics <team@crowdbotics.com>
2022-06-14 10:24:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shampoo_show_35720/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shampoo_show_35720;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shampoo_show_35720";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shampoo_show_35720",
+  url: "https://shampoo_show_35720.botics.co",
+  api: "https://shampoo_show_35720.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
